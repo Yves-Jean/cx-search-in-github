4fa9b12a05c1b3d0ef81e46d78b5a80207a83981
Yves kouayip <kamwayves37@gmail.com>
2020-12-07 23:25:19 +0100

    defined a feature swith theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,22 @@
 import { SearchBar, UserDetails, Toolbar } from "./components";
+import { ThemeContext } from "./contexts/ThemeContext";
+import React, { useState } from "react";
 
 import "./scss/style.scss";
 import "./App.scss";
 
 function App() {
+  const [theme, setTheme] = useState("light");
   return (
-    <div className="container">
-      <Toolbar />
-      <main className="display-flex flex-justify-center mg-tp-4">
-        <SearchBar />
-        <UserDetails />
-      </main>
-    </div>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <div className="container">
+        <Toolbar />
+        <main className="display-flex flex-justify-center mg-tp-4">
+          <SearchBar />
+          <UserDetails />
+        </main>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- client/src/components/toolbar/Toolbar.jsx
+++ client/src/components/toolbar/Toolbar.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { ThemeContext } from "../../contexts/ThemeContext";
+import React, { useContext } from "react";
 
 import "./toolbar.scss";
 
 const Toolbar = () => {
+  const [theme, setTheme] = useContext(ThemeContext);
+  const getSwithThemeName = () => (theme === "light" ? "dark" : "light");
+  const swithTheme = () => {
+    const bodyClass = document.querySelector("body").classList;
+    bodyClass.remove(`theme-${theme}`);
+    bodyClass.add(`theme-${getSwithThemeName()}`);
+    setTheme(getSwithThemeName());
+  };
   return (
     <header className="header">
       <nav className="nav-bar display-flex flex-justify-end pd-y-2">
-        <button className="btn btn-pink">Theme</button>
+        <button className="btn btn-pink" onClick={swithTheme}>
+          {getSwithThemeName()} theme
+        </button>
       </nav>
     </header>
   );
