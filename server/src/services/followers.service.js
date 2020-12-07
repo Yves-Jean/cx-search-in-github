5c97326c5e0fe86c1207ca7f01bcb05fbbb277c9
jeanPcr <jean.coding971@gmail.com>
2020-12-07 16:30:35 +0100

    init services

---
+++ server/src/services/followers.service.js
@@ -0,0 +1,7 @@
+// export const getFollowers = (username) => {};
+
+// export const insertFollower = (user) => {};
+
+// export const updatetFollowers = (id, user) => {};
+
+// export const deletetFollower = (id) => {};
---
+++ server/src/services/user.service.js
@@ -0,0 +1,11 @@
+const getUser = (username) => {};
+const insertUser = (user) => {};
+const updatetUser = (id, user) => {};
+const deletetUser = (id) => {};
+
+module.exports = {
+  getUser,
+  insertUser,
+  updatetUser,
+  deletetUser,
+};
