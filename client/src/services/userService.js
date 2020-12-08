ed5890c33c763c44f5fbf3e0616f8b5c74ef6a62
Yves kouayip <kamwayves37@gmail.com>
2020-12-08 20:36:03 +0100

    update style

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,5 +1,6 @@
 const url = process.env.REACT_APP_SEARCH_IN_GITHUB_API;
 
+console.log(url);
 const getUser = (username) => {
   return fetch(`${url}/users/${username}`).then((res) => res.json());
 };
